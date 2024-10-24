f34364b3d03e32808de8fb3407a6950f839fffbd
Luca Neumann <81520529+luca-neumann@users.noreply.github.com>
2024-10-24 11:08:15 +0200

    Update Poker_Aufgabe.py

--- Poker_Aufgabe.py
+++ Poker_Aufgabe.py
@@ -13,7 +13,7 @@ import random
 #royal flush 0,000154%
 
 karten = 52
-spiele_anzahl = 100000
+spiele_anzahl = 10000
 
 #Liste von 0-51, für jede Karte
 zahlen = [i for i in range(0, karten)]
@@ -38,12 +38,15 @@ def ziehe_fuenf_karten():
     gezogene_zahlen = []
     for i in range(5):
         #wählt eine Karte aus dem Deck aus
+        #-i damit nicht nochmal die gleiche Karte gezogen wird
         random_index = random.randint(0, len(zahlen) - 1 - i)
         #Modulo 13 um nur den Wert der Karte zu bekommen
+        #13 weil es 13 verschiedene Werte gibt
+        #0 für Ass, 10 für Bube, 11 für Dame, 12 für König
         gezogene_zahl = zahlen[random_index] % 13
         #kommt in die Liste
         gezogene_zahlen.append(gezogene_zahl)
-        #tauscht die gz. Karte mit der letzten Karte aus
+        #tauscht die gz. Karte mit der letzten Karte aus und verkleinert
         #somit gibt es keine Karten doppelt
         zahlen[random_index], zahlen[len(zahlen) - 1 - i] = zahlen[len(zahlen) - 1 - i], zahlen[random_index]
 
@@ -83,7 +86,7 @@ def erkenne_kombinationen(hand):
     elif paar:
         return "Paar"
     else:
-        return "Du Looser"
+        return None
 
 #für des halt so oft aus wie oben angegeben
 def spiele_poker_simulation(spiele_anzahl):
@@ -100,4 +103,4 @@ print(f"Ergebnisse nach {spiele_anzahl} Spielen:")
 #diese for geht durch jedes Element in dem Wörterbuch
 for kombination, anzahl in wahrscheinlichkeiten_statistik.items():
     prozent = (anzahl / spiele_anzahl) * 100
-    print(f"{kombination}: {anzahl} Mal ({prozent:.4f}%)")
+    print(f"{kombination}: {anzahl} Mal ({prozent:.4f}%)") #.4f weil 4 Nachkommastellen, f für Float
